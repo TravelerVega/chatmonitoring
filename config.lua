34268b17307006b7ea212f65c569b535adb6598c
TravelerVega <51480428+TravelerVega@users.noreply.github.com>
2020-07-20 16:56:15 +0200

    memhg

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 config = {}
 
 config.max_delay = 5
-config.other_chat = true -- set to true if you have set up a diffrent chat thingy, for example rolesFX.
+config.other_chat = false -- set to true if you have set up a diffrent chat thingy, for example rolesFX.
